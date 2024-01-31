96712538bf0e18cfa81a9f8d40cf577e0b17f0fd
EstelleDab <152979607+EstelleDab@users.noreply.github.com>
2024-01-31 17:20:51 +0100

    Update SearchController.php

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Controller\ArticleController;
 use App\Form\SearchArticleFormType;
 
 class SearchController extends AbstractController
