f2845032a2088263fc1c67842fc1c1a616027da8
EstelleDab <152979607+EstelleDab@users.noreply.github.com>
2024-02-01 11:07:32 +0100

    Update SearchArticleFormType.php

--- src/Form/SearchArticleFormType.php
+++ src/Form/SearchArticleFormType.php
@@ -3,7 +3,10 @@
 namespace App\Form;
 
 use App\Entity\Article;
+use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
