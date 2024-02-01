9e35bffb9c796877f3a61719f0eac89dfecfa830
EstelleDab <152979607+EstelleDab@users.noreply.github.com>
2024-02-01 12:02:15 +0100

    Update SearchController.php

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -11,20 +11,23 @@ use App\Controller\ArticleController;
 
 class SearchController extends AbstractController
 {
-    #[Route('/search', name: 'app_search')]
-
-
+    #[Route('/search', name: 'app_search', methods: ['GET'])]
 
     public function index(): Response
     {
-        $form = $this->createForm(SearchArticleFormType::class);  
-        return $this->renderForm('search/index.html.twig', [
-            'form' => $form,
+        $form = $this->createForm(SearchArticleFormType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            return $this->render('search/index.html.twig', [
+                'articles' => $articleRepository->findAll(),
+            ]);
+
+        }
+
+        return $this->render('search/index.html.twig', [
+        'form' => $form->createView()
             
         ]);
-        
     }
-
-
-
 }
