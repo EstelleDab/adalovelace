04fd213202831157be7bc6afd486e571a536634b
EstelleDab <152979607+EstelleDab@users.noreply.github.com>
2024-02-02 16:39:03 +0100

    Update ArticleController.php

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -80,8 +80,8 @@ class ArticleController extends AbstractController
         $form = $this->createFormBuilder()
             ->add('category', ChoiceType::class, [
                 'choices' => [
-                    'Ciel !' => '1',
-                    'Ada Lovelace Team' => '2',
+                    'Ciel !' =>'1',
+                    'Ada Lovelace Team' =>'2',
                 ],
             ])
             ->add('query', TextType::class)
